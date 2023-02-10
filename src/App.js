644342d3434a4d86937d6f6d79228b136da7a159
ellenbechtel <ellen.bechtel@gmail.com>
2023-02-10 15:57:13 -0500

    switch to hash

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { ThemeContext } from './contexts/theme'
 
 import Header from './components/Header/Header'
 import Home from './views/Home/Home'
-import CaseStudy1 from './views/CaseStudies/CaseStudy1'
-import CaseStudy2 from './views/CaseStudies/CaseStudy2'
-import CaseStudy3 from './views/CaseStudies/CaseStudy3'
+// import CaseStudy1 from './views/CaseStudies/CaseStudy1'
+// import CaseStudy2 from './views/CaseStudies/CaseStudy2'
+// import CaseStudy3 from './views/CaseStudies/CaseStudy3'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 
 import Footer from './components/Footer/Footer'
@@ -21,10 +21,10 @@ const App = () => {
       <Header />
 
       <Routes>
-        <Route path="./" element={ <Home /> } />
-        <Route path="casestudy1" element={ <CaseStudy1/> } />
+        <Route path="" element={ <Home /> } />
+        {/* <Route path="casestudy1" element={ <CaseStudy1/> } />
         <Route path="casestudy2" element={ <CaseStudy2/> } />
-        <Route path="casestudy3" element={ <CaseStudy3/> } />
+        <Route path="casestudy3" element={ <CaseStudy3/> } /> */}
       </Routes>
       
 
